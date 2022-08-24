f470ce9bcd39176cd8e64050063dcb81afc4806e
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2022-08-24 22:19:15 +0200

    Remove debug prints

--- http/backend.v
+++ http/backend.v
@@ -13,11 +13,9 @@ fn (req &Request) ssl_do(port int, method Method, host_name string, path string)
 	mut ssl_conn := tlse.new_ssl_conn()
 	ssl_conn.connect(host_name, port)
 	req_headers := req.build_request_headers(method, host_name, path)
-	println(req_headers)
 	ssl_conn.write(req_headers.bytes()) or { return err }
 	mut response_bytes := []u8{len: 0xFFFF}
 	ssl_conn.read(mut response_bytes)
 	response_text := response_bytes.bytestr()
-	println(response_text)
 	return parse_response(response_text)
 }
